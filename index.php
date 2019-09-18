ebf153e20f424853346a19371d4013681f6c33f0
Edgar Reyes <54544372+iLata@users.noreply.github.com>
2019-09-18 12:27:58 -0500

    Se agregó la interfaz para guardar usuarios

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+    include('./src/scripts/php/db.php');
+?>
+
+<?php 
+    include('./src/includes/header.php');
+?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-body">
+                <form>
+                    <div class="form-group">
+                        <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre de la persona" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="edad" id="edad" class="form-control" placeholder="Edad de la persona">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="direccion" id="direccion" class="form-control" placeholder="Dirección de la persona">
+                    </div>
+                    <button id="insertUser" type="button" class="btn btn-success btn-block">Guardar</button>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-8">
+
+        </div>
+    </div>
+</div>
+
+<?php
+    include('./src/includes/footer.php');
+?>
---
+++ src/scripts/php/db.php
@@ -0,0 +1,12 @@
+<?php
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'abc'
+    );
+
+    /*if(isset($conn)){
+        echo "DB IS CONNECT";
+    }*/
+?>
